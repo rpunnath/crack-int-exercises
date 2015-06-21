5cd7778ca33640abb7261b1fb9f4275de851d1c3
rpunnath <gitpunnath@outlook.com>
2015-06-20 23:31:59 -0700

    1.1

---
+++ Exercises/CrackingCoding/Chapter1/Questions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    public static class Questions
+    {
+        public static void HasUniqueCharacters()
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Input String");
+                input = Console.ReadLine();
+                Console.WriteLine("Unique {0}, Continue Y/N?"
+                    , StringOperations.AreAllUniqueVersion(input) ? "Yes" : "No");
+                input = Console.ReadLine();
+            } while (!input.Equals("N", StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
