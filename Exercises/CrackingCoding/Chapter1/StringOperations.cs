2a1f235f21cfe1f58bf29ef355a745729abcae0c
rpunnath <gitpunnath@outlook.com>
2015-06-20 23:30:40 -0700

    1.1

---
+++ Exercises/CrackingCoding/Chapter1/StringOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    static class StringOperations
+    {
+
+        public static bool AreAllUniqueVersion(string sampleString)
+        {
+            try
+            {
+                int len = sampleString.Length;
+                if (sampleString.Length <= 1)
+                    return true;
+
+                if (sampleString.Equals(sampleString.Substring(1) + sampleString.Substring(0, 1)))
+                    return true;
+
+                return false;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return false;
+            }
+        }
+    }
+}
